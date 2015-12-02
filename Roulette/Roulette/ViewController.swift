d242e9ac960b2eafc656d401b26326c00dddba58
Wojtek Czekalski <me@wczekalski.com>
2015-12-02 01:54:07 +0100

    Added circle to the app and implemented basic interaction

--- Roulette/Roulette/ViewController.swift
+++ Roulette/Roulette/ViewController.swift
@@ -11,25 +11,55 @@ import CoreGraphics
 
 class ViewController: UIViewController {
     
+    let gestureRecognizer = UIPanGestureRecognizer()
+    let rouletteView = UIView(frame: CGRect(origin: CGPointZero, size: CGSize(width: 300, height: 300)))
+    var lastTranslationFromCenter: CGPoint = CGPointZero
+    
     override func viewDidLoad() {
         
-        let r = RouletteGeometry(frame: CGRect(x: 50, y: 50, width: 300, height: 300), width: 60)
+        let r = RouletteGeometry(frame: CGRect(x: 0, y: 0, width: 300, height: 300), width: 60)
         
-        let layers = [Int](0..<4).map { i in
-            return Segment(numberOfSegments: 4, segmentIndex: i)
-        }.map { s in
-            return segment(r, segment: s)
-        }.map { (path: UIBezierPath) -> CAShapeLayer in
-            let s = path.shapeLayer()
-            s.frame = r.bounds
-            s.fillColor = UIColor.greenColor().CGColor
-            s.strokeColor = UIColor.blackColor().CGColor
-            return s
+        let layers = r.donut().split(4).map { s in
+            return s.bezierPath().shapeLayer()
         }
-        
+
         for l in layers {
-            self.view.layer.addSublayer(l)
+            l.frame = r.bounds
+            l.strokeColor = UIColor.blackColor().CGColor
+            l.fillColor = UIColor.clearColor().CGColor
+            rouletteView.layer.addSublayer(l)
         }
+        
+        gestureRecognizer.addTarget(self, action: "rotated")
+        gestureRecognizer.maximumNumberOfTouches = 1
+        
+        rouletteView.layer.borderWidth = 1
+        
+        rouletteView.addGestureRecognizer(gestureRecognizer)
+        self.view.addSubview(rouletteView)
+        
+    }
+    
+    @objc func rotated() {
+        
+        guard gestureRecognizer.state != .Ended else {
+            lastTranslationFromCenter = CGPointZero
+            return
+        }
+        
+        let point = gestureRecognizer.locationInView(rouletteView)
+        let center = rouletteView.bounds.center()
+        let previousTouchPoint = center.translate(lastTranslationFromCenter)
+        let angle = Angle(a: previousTouchPoint, b: point, vertex: center)
+        
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        for l in rouletteView.layer.sublayers! {
+            l.transform = CATransform3DRotate(l.transform, angle.measure(), 0, 0, 1)
+        }
+        CATransaction.commit()
+        
+        lastTranslationFromCenter = point.translationFromPoint(center)
     }
 }
 
